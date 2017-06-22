5fd9e12d5b96406ac9e97c6f44492e60c12d5ece
mac <mac@MacBook-Pro-di-mac.local>
2017-06-22 11:00:33 +0200

    login ok

--- Main/Login.swift
+++ Main/Login.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class Login: MYViewController {
+class Login: MYViewController, UITextFieldDelegate {
     class func Instance() -> Login {
         let vc = self.load(storyboardName: "Main") as! Login
         return vc
     }
+    @IBOutlet var userView: UIView!
+    @IBOutlet var passView: UIView!
     
     @IBOutlet var userText: MYTextField!
     @IBOutlet var passText: MYTextField!
@@ -20,18 +22,35 @@ class Login: MYViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.userView.layer.cornerRadius = self.userView.frame.size.height / 2
+        self.passView.layer.cornerRadius = self.passView.frame.size.height / 2
+        
         self.userText.text = "Utente"
         self.passText.text = "Passwd"
     }
     
     @IBAction func loginTapped () {
         if self.userText.text!.isEmpty {
+            self.userText.becomeFirstResponder()
             return
         }
         if self.passText.text!.isEmpty {
+            self.passText.becomeFirstResponder()
             return
         }
+        self.view.endEditing(true)
         self.checkUser()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.userText {
+            self.passText.becomeFirstResponder()
+            return true
+        }
+        if textField == self.passText {
+            self.view.endEditing(true)
+        }
+        return true
     }
     
     private func checkUser () {
@@ -45,15 +64,21 @@ class Login: MYViewController {
             "password"     : self.passText.text!,
         ]
         request.start() { (result, response) in
-            print (response)
-            self.httpResponse(dict: [:])
+            self.httpResponse(dict: response)
         }
     }
     
     private func httpResponse (dict: JsonDict) {
-        User.shared.saveUser(user: self.userText.text!)
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "Main")
-        self.navigationController?.show(vc!, sender: self)
-
+        print (dict)
+        let code = dict.int("code")
+//        let status = dict.string("status")
+        let msg = dict.string("message")
+        if code == 200 {
+            User.shared.saveUser(user: self.userText.text!)
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "Main")
+            self.navigationController?.show(vc!, sender: self)
+            return
+        }
+        self.alert("Error: \(code)", message: msg, okBlock: nil)
     }
 }
